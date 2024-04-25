899d7d678705e76d1a777133c306d0caf925213b
LeinadMori <iromdaniel@gmail.com>
2024-04-25 14:26:37 +0100

    Corrected some code in the task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,13 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n - 1)
-
-def binomial_coefficient(n, k):
-    return factorial(n) // (factorial(k) * factorial(n - k))
-
 def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []
-    for i in range(n):
-        row = []
-        for j in range(i + 1):
-            row.append(binomial_coefficient(i, j))
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
         triangle.append(row)
-    
+
     return triangle
